52e11505a29c29bdfe4df298952744cfd57b3907
Greatnex1 <noahgreatakoni@gmail.com>
2025-02-24 21:14:57 +0100

    add user feature

--- src/main/java/com/greatnex/semicolon_task/infrastructure/adapters/output/persistence/UserPersistenceAdapter.java
+++ src/main/java/com/greatnex/semicolon_task/infrastructure/adapters/output/persistence/UserPersistenceAdapter.java
@@ -1,12 +1,22 @@
 package com.greatnex.semicolon_task.infrastructure.adapters.output.persistence;
 
 import com.greatnex.semicolon_task.application.ports.output.GetUserFullNameOutputPort;
+import com.greatnex.semicolon_task.application.ports.output.PlatformUserOutputPort;
+//import com.greatnex.semicolon_task.domain.exception.UserException;
+import com.greatnex.semicolon_task.domain.exception.UserException;
+import com.greatnex.semicolon_task.domain.messages.ErrorMessages;
 import com.greatnex.semicolon_task.domain.models.PlatformUser;
 import com.greatnex.semicolon_task.infrastructure.adapters.output.persistence.dao.UserName;
+import com.greatnex.semicolon_task.infrastructure.adapters.output.persistence.entity.PlatformUserEntity;
+import com.greatnex.semicolon_task.infrastructure.adapters.output.persistence.mapper.PlatformUserMapper;
 import com.greatnex.semicolon_task.infrastructure.adapters.output.persistence.repository.PlatformUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.keycloak.events.Errors;
+import org.springframework.boot.web.server.Http2;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -14,12 +24,28 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class UserPersistenceAdapter implements GetUserFullNameOutputPort {
+public class UserPersistenceAdapter implements GetUserFullNameOutputPort, PlatformUserOutputPort {
 
     private final PlatformUserRepository userRepository;
-    @Override
-    public void save(PlatformUser platformUser) {
+    private final PlatformUserMapper platformUserMapper;
 
+    @Override
+    public PlatformUser save(PlatformUser platformUser) {
+        platformUser.validateUserRequiredData();
+   PlatformUserEntity userEntity = platformUserMapper.toPlatformUserEntity(platformUser);
+    userRepository.save(userEntity);
+        log.info("Saved user identity {}",platformUser);
+     return platformUserMapper.toPlatformUser(userEntity);
+    }
+
+    @Override
+    public PlatformUser findUserById(String id) throws UserException {
+        if (StringUtils.isEmpty(id)) {
+            PlatformUserEntity foundUser = userRepository.findById(id).orElseThrow(() -> new UserException(
+                    ErrorMessages.USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+            return platformUserMapper.toPlatformUser(foundUser);
+        }
+            throw new UserException(String.format(ErrorMessages.NULL_OBJECT));
     }
 
     @Override
