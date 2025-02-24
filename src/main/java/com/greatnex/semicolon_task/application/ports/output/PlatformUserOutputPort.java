a02787e3c016c2732b1e3b231b92f46cb2bebe0c
Greatnex1 <noahgreatakoni@gmail.com>
2025-02-24 21:39:32 +0100

    add user feature test

--- src/main/java/com/greatnex/semicolon_task/application/ports/output/PlatformUserOutputPort.java
+++ src/main/java/com/greatnex/semicolon_task/application/ports/output/PlatformUserOutputPort.java
@@ -1,7 +1,11 @@
 package com.greatnex.semicolon_task.application.ports.output;
 
+//import com.greatnex.semicolon_task.domain.exception.UserException;
+import com.greatnex.semicolon_task.domain.exception.UserException;
 import com.greatnex.semicolon_task.domain.models.PlatformUser;
 
 public interface PlatformUserOutputPort {
-    void save(PlatformUser platformUser);
+    PlatformUser save (PlatformUser platformUser);
+   PlatformUser findUserById(String id) throws UserException;
+    boolean userExists(String id);
 }
