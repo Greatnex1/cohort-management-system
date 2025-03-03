1be92ae5c16f3e5b044a889ba3e8622077358eaa
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-03 03:51:46 +0100

    add logic

--- src/main/java/com/greatnex/semicolon_task/domain/service/CohortService.java
+++ src/main/java/com/greatnex/semicolon_task/domain/service/CohortService.java
@@ -42,9 +42,9 @@ public class CohortService implements CohortUseCase {
             }
             cohort.setDateCreated(ZonedDateTime.now());
 
-//            if(ObjectUtils.isNotEmpty(identity)) {
-//                cohort.setCreatedBy(identity.getId());
-//            }else throw new  IllegalArgumentException(String.format(ErrorMessages.EMPTY_INPUT_ERROR, "created by"));
+            if(ObjectUtils.isNotEmpty(identity)) {
+                cohort.setCreatedBy(identity.getId());
+            }else throw new  IllegalArgumentException(String.format(ErrorMessages.EMPTY_INPUT_ERROR, "created by"));
 
             cohort.validateCohortData();
             log.info("Cohort info: {}", cohort.getName());
