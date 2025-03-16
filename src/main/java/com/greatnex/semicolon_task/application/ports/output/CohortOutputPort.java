10f905226c787a5816ac78b0cc18cc9ced5acbb2
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-16 23:28:21 +0100

    add cohort port

--- src/main/java/com/greatnex/semicolon_task/application/ports/output/CohortOutputPort.java
+++ src/main/java/com/greatnex/semicolon_task/application/ports/output/CohortOutputPort.java
@@ -1,14 +1,14 @@
 package com.greatnex.semicolon_task.application.ports.output;
 
 import com.greatnex.semicolon_task.domain.models.Cohort;
-import com.greatnex.semicolon_task.domain.exception.CohortException;
+import com.greatnex.semicolon_task.domain.exception.GenericException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CohortOutputPort {
     Cohort saveCohortDetails(Cohort cohort);
-    Cohort findCohortById(String cohortId) throws CohortException;
+    Cohort findCohortById(String cohortId) throws GenericException;
     Page<Cohort> findAllCohort(Pageable pageable);
-    void deleteCohort(String id) throws CohortException;
+    void deleteCohort(String id) throws GenericException;
     boolean checkCohortNameAlreadyExist(String id);
 }
