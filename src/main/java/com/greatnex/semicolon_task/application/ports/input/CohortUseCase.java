6aebec00a38f3a6e4b587d66828699811eccbccf
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-19 06:24:32 +0100

    add cohort interface

--- src/main/java/com/greatnex/semicolon_task/application/ports/input/CohortUseCase.java
+++ src/main/java/com/greatnex/semicolon_task/application/ports/input/CohortUseCase.java
@@ -1,13 +1,13 @@
 package com.greatnex.semicolon_task.application.ports.input;
 
 import com.greatnex.semicolon_task.domain.models.Cohort;
-import com.greatnex.semicolon_task.domain.exception.CohortException;
+import com.greatnex.semicolon_task.domain.exception.GenericException;
 import com.greatnex.semicolon_task.domain.models.PlatformUser;
 import org.springframework.data.domain.Page;
 
 public interface CohortUseCase {
-    Cohort createCohort(PlatformUser identity, Cohort cohort) throws CohortException;
-    Cohort viewCohortDetails(String cohortId) throws CohortException;
+    Cohort createCohort(PlatformUser identity, Cohort cohort) throws GenericException;
+    Cohort viewCohortDetails(String cohortId) throws GenericException;
     Page<Cohort> viewAllCohorts(int page, int size) ;
 
 
