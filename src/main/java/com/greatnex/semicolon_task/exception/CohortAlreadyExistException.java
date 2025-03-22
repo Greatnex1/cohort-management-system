900afff451321d561dbab9d40983ee8ca527f9cc
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-22 20:09:42 +0100

    deleted

--- src/main/java/com/greatnex/semicolon_task/exception/CohortAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.greatnex.semicolon_task.exception;
-
-public class CohortAlreadyExistException extends Exception {
-    public CohortAlreadyExistException(String message) {
-        super   (message);
-    }
-}
