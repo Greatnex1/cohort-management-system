6940c1766e290b33c4ab1a1d2b529ee95c3a58ee
Greatnex1 <noahgreatakoni@gmail.com>
2025-03-22 20:10:20 +0100

    deleted

--- src/main/java/com/greatnex/semicolon_task/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greatnex.semicolon_task.repository;
-
-import com.greatnex.semicolon_task.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
